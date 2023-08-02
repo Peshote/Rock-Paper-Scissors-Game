163f6111ef26b3eab92348e13731167cb68420e9
Petar Iliev <peshote@gmail.com>
2023-08-02 22:02:07 +0300

    Add colors to console lines

--- Project-Rock-Paper-Scissors/Rock-Paper-Scissors.cs
+++ Project-Rock-Paper-Scissors/Rock-Paper-Scissors.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 namespace Project_Rock_Paper_Scissors
 {
@@ -11,7 +10,9 @@ namespace Project_Rock_Paper_Scissors
             const string Paper = "Paper";
             const string Scissors = "Scissors";
 
+            Console.ForegroundColor = ConsoleColor.Cyan; // Set foreground color to Cyan
             Console.Write("Choose [r]ock, [p]aper or [s]cissors: ");
+            Console.ResetColor(); // Reset foreground color to default
 
             string playerMove = Console.ReadLine();
 
@@ -19,11 +20,11 @@ namespace Project_Rock_Paper_Scissors
             {
                 playerMove = Rock;
             }
-            else if (playerMove == "p" ||  playerMove == "paper")
+            else if (playerMove == "p" || playerMove == "paper")
             {
                 playerMove = Paper;
             }
-            else if(playerMove == "s" ||  playerMove == "scissors")
+            else if (playerMove == "s" || playerMove == "scissors")
             {
                 playerMove = Scissors;
             }
@@ -33,7 +34,9 @@ namespace Project_Rock_Paper_Scissors
                 return;
             }
 
+            Console.ForegroundColor = ConsoleColor.Green; // Set foreground color to Green
             Console.WriteLine($"You chose: {playerMove}.");
+            Console.ResetColor(); // Reset foreground color to default
 
             Random random = new Random();
             int computerRandomNumber = random.Next(1, 4);
@@ -53,19 +56,27 @@ namespace Project_Rock_Paper_Scissors
                     break;
             }
 
+            Console.ForegroundColor = ConsoleColor.Magenta; // Set foreground color to Magenta
             Console.WriteLine($"The computer chose: {computerMove}.");
+            Console.ResetColor(); // Reset foreground color to default
 
-            if ((playerMove == Rock &&  computerMove == Scissors) || (playerMove == Paper && computerMove == Rock) || (playerMove == Scissors && computerMove == Paper))
+            if ((playerMove == Rock && computerMove == Scissors) || (playerMove == Paper && computerMove == Rock) || (playerMove == Scissors && computerMove == Paper))
             {
+                Console.ForegroundColor = ConsoleColor.Yellow; // Set foreground color to Yellow
                 Console.WriteLine("You win!");
+                Console.ResetColor(); // Reset foreground color to default
             }
-            else if ((playerMove == Rock && computerMove == Paper) ||  (playerMove == Paper && computerMove == Scissors) || (playerMove == Scissors && computerMove == Rock))
+            else if ((playerMove == Rock && computerMove == Paper) || (playerMove == Paper && computerMove == Scissors) || (playerMove == Scissors && computerMove == Rock))
             {
+                Console.ForegroundColor = ConsoleColor.Red; // Set foreground color to Red
                 Console.WriteLine("You lose!");
+                Console.ResetColor(); // Reset foreground color to default
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Blue; // Set foreground color to Blue
                 Console.WriteLine("It's a Draw!");
+                Console.ResetColor(); // Reset foreground color to default
             }
         }
     }
